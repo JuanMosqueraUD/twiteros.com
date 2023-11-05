d031877b1348ae2d858c3e5db6cf8d1c29cfa9d8
AnaTuPatrona <111031121+AnaTuPatrona@users.noreply.github.com>
2023-11-04 22:51:08 -0500

    dataset con la info necesaria jeje
    favor probar, parceritos

--- TwitterReto.py
+++ TwitterReto.py
@@ -24,8 +24,15 @@ def conexion(APIKey,APISecret,accessToken,accessTokenSecret):
 
 api=conexion(*getKeys())
 
-#client = tweepy.Client('AAAAAAAAAAAAAAAAAAAAAEneqgEAAAAAh1fYx4U38oXmstXpvWVNX9Ov4iY%3DEXix8mmOriaXshZsApRMRZk34QL9eeagpzJesOzeL0RXolBmmR')
-data=api.get_user(screen_name="GustavoBolivar")
-#data=api.get_user(screen_name="Daniel29468195")
-print (json.dumps(data._json,indent=2))
+data=[api.get_user(screen_name="GustavoBolivar"),api.get_user(screen_name="CarlosFGalan"),api.get_user(screen_name="Diego_Molano"),api.get_user(screen_name="JERobledo"),api.get_user(screen_name="ElGeneralVargas"),api.get_user(screen_name="JDOviedoA"),api.get_user(screen_name="Rodrigo_Lara_")]
 
+gustavoBolivar=[data[0].followers_count, data[0].favourites_count, data[0].listed_count]
+carlosGalan=[data[1].followers_count, data[1].favourites_count, data[1].listed_count]
+diegoMolano=[data[2].followers_count, data[2].favourites_count, data[2].listed_count]
+jorgeRobledo=[data[3].followers_count, data[3].favourites_count, data[3].listed_count]
+generalVargas=[data[4].followers_count, data[4].favourites_count, data[4].listed_count]
+juanOviedo=[data[5].followers_count, data[5].favourites_count, data[5].listed_count]
+rodrigoLara=[data[6].followers_count, data[6].favourites_count, data[6].listed_count]
+
+candidatos=pd.DataFrame([gustavoBolivar,carlosGalan,diegoMolano,jorgeRobledo,generalVargas,juanOviedo,rodrigoLara],columns=["Seguidores","Favoritos","N°Listas"],index=["Gustavo Bolivar","Carlos Galan","Diego Molano","Jorge Robledo","Jorge Luis Vargas","Juan Oviedo","Rodrigo Lara"])
+print(candidatos)
