3f415cb6449c5e71b2da7a3ac803eb29abb3da35
Daniel141204 <67084296+Daniel141204@users.noreply.github.com>
2023-11-09 00:24:36 -0500

    fusion de la fusion
    la que quite en views sin saber bien que hacia, ya la fusioné y está todo ok

--- playround/views.py
+++ playround/views.py
@@ -3,10 +3,7 @@ from django.http import HttpResponse
 from playround.TwitterReto import *
 # Create your views here.
 
-
-
-
-
 def datos(request):
+    crear_candidatos_desde_dataframe()
     candidatos = Candidato.objects.all()
     return render(request, 'index.html', {'candidatos': candidatos})
