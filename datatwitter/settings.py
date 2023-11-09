cf0911e297008ca3d39b2e01c130b8218f3b862c
Daniel141204 <Danifelzarbel@gmail.com>
2023-11-08 19:20:42 -0500

    diavlo pallo ayudanos
    hay q hacer las migraciones

--- datatwitter/settings.py
+++ datatwitter/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datatwitter.settings')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
